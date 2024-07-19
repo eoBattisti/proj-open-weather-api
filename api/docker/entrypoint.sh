cd629e5b3421a92be6328d0a4022c0d3bdd56729
Nicolas Battisti <nickgabrielbd@gmail.com>
2024-07-19 17:17:28 -0300

    feat(docker): removed entrypoint.sh unused command

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-database="$POSTGRES_DATABASE"
-host="$POSTGRES_HOST"
-
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -d "$database" -U "$POSTGRES_USER" -c '\q'; do
-    echo "Postgres is unavailable - sleeping"
-    sleep 1
-done
 
 echo "Starting FastAPI application as `whoami` in mode $MODE"
 uvicorn --reload --host $HOST --port $PORT main:app
