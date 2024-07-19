92a092102c1e4a86492f49909a4ead827c972589
Nicolas Battisti <nickgabrielbd@gmail.com>
2024-07-19 17:29:35 -0300

    feat(api): add api information

--- api/main.py
+++ api/main.py
@@ -1,7 +1,13 @@
 from fastapi.applications import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from weather.router import router as weather_router 
 
-app = FastAPI()
+app = FastAPI(
+    title="OpenWeather microservice",
+    version="0.1.0",
+    description="API to fetch weather data from OpenWeather and show the progress of the requests",
+    default_response_class=ORJSONResponse
+)
 
 app.include_router(router=weather_router)
